773bdd28eea630760ca4d0abc07faeaeab5a7257
brianna-salinas <zevensos@gmail.com>
2026-06-25 17:53:28 -0500

    initial commit Gaaaaa

---
+++ src/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+
+mongoose.connect('mongodb://mongo/mydatabase',{ 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+    .then(db => console.log('Database connected to', db.connection.host))
+    .catch(err => console.error(err));
+
