ab4002fa9c725df1e111814b79cad3294d2ec9f4
brianna-salinas <zevensos@gmail.com>
2026-06-28 23:23:48 -0500

    feat: backend completo con auth

--- src/database.js
+++ src/database.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-// ENV DEV (local)
-mongoose.connect("mongodb://localhost:27018/modyn_db")
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27018/modyn_db")
 .then(db => console.log("DB is connected to ", db.connection.host))
 .catch(err => console.error(err));
